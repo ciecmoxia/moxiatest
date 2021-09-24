dffad2bda4f795d849fcf6777b587786eb130d63
ciecmoxia <ciecmoxia@163.com>
2021-09-24 16:12:08 +0800

    Day14
    开始学习Xml

--- Month1/XML01.py
+++
@@ -1,7 +0,0 @@
-'''
-一、理解
-标记语言，是一种将文本（ Text）以及文本相关的其他信息结合起来，展现出关
-于文档结构和数据处理细节的电脑文字编码。当今广泛使用的标记语言是超文本
-标记语言（HyperText Markup Language， HTML）和可扩展标记语言 (Extensible
-Markup Language XML)。标记语言广泛应用于网页和网络应用程序。
-'''
